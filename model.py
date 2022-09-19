54a5129a7fa48e95684ee336be822d03f9b744b9
Ch3nYe <sud0su@qq.com>
2022-09-19 18:04:37 +0800

    a new model definition

--- model.py
+++ model.py
@@ -46,3 +46,39 @@ class VulnClassifier(nn.Module):
             'privilege_required': privilege_required, 'attack_vector': attack_vector,
             'impact_1': impact_1, 'impact_2': impact_2, 'impact_3': impact_3
         }
+
+
+class VulnClassifier2(nn.Module):
+    def __init__(self,
+                 BERT_name=None,
+                 bert_num_hidden_layers=1,
+                 bert_num_attention_heads=6
+                 ):
+        super().__init__()
+        if BERT_name == None:
+            self.bert = BertModel(BertConfig(num_hidden_layers=bert_num_hidden_layers,
+                                             num_attention_heads=bert_num_attention_heads)
+                                  )
+        else:
+            self.bert = BertModel.from_pretrained(BERT_name)
+        self.out_privilege_required = nn.Linear(self.bert.config.hidden_size, 4)
+        self.out_attack_vector = nn.Linear(self.bert.config.hidden_size, 2)
+        self.out_impact_1 = nn.Linear(self.bert.config.hidden_size, 5)
+        self.out_impact_2 = nn.Linear(self.bert.config.hidden_size, 7)
+        self.out_impact_3 = nn.Linear(self.bert.config.hidden_size, 4)
+
+    def forward(self, input_ids, attention_mask):
+        _, pooled_output = self.bert(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            return_dict = False
+        )
+        privilege_required = self.out_privilege_required(pooled_output)
+        attack_vector = self.out_attack_vector(pooled_output)
+        impact_1 = self.out_impact_1(pooled_output)
+        impact_2 = self.out_impact_2(pooled_output)
+        impact_3 = self.out_impact_2(pooled_output)
+        return {
+            'privilege_required': privilege_required, 'attack_vector': attack_vector,
+            'impact_1': impact_1, 'impact_2': impact_2, 'impact_3': impact_3
+        }
